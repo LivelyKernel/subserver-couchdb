d053e11ee72ea7f9f4e56c6b03e1dda08498d8aa
Robert Krahn <robert.krahn@gmail.com>
2013-01-14 17:09:57 -0800

    test for coudb install and start couchdb process

--- index.js
+++ index.js
@@ -1,5 +1,46 @@
-module.exports = function(basepath, app) {
-    app.get(basepath, function(req, res) {
-        res.send({name: 'CouchDB subserver'})
-    })
+var shelljs = require('shelljs/global'),
+    spawn = require('child_process').spawn,
+    cradle = require('cradle'),
+    serverInfo = {name: 'CouchDB subserver'},
+    isWorking = true,
+    errorMessage = "No error";
+
+
+
+function testIfCouchDBInstalled() {
+    if (!which('couchdb')) {
+        errorMessage = "CouchDB subserver not working: commandline tool couchdb required.";
+        isWorking = false;
+    }
 }
+
+function startCouchDB(thenDo) {
+    var proc = spawn("couchdb");
+    proc.stderr.on("data", function(data) { echo(data.toString()); });
+    proc.stdout.on("data", function(data) { echo(data.toString()); });
+    proc.on("exit", function(data) {
+        echo("CouchDB process stopped.");
+        isWorking = false;
+    });
+}
+
+function main() {
+    testIfCouchDBInstalled();
+
+    if (isWorking) {
+        serverInfo.status = "active";
+        startCouchDB();
+    } else {
+        echo(errorMessage);
+        serverInfo.status = "inactive";
+        serverInfo.error = errorMessage;
+    }
+
+    module.exports = function(basepath, app) {
+        app.get(basepath, function(req, res) {
+            res.send(serverInfo);
+        })
+    }
+}
+
+main();
